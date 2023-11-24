a0bb73839e4ff2e683b1c15dda123645f6d9b155
ElJoamy <josephsalguero1@upb.edu>
2023-11-24 11:12:54 -0400

    Add LinkedIn message prompt

---
+++ prompts.py
@@ -0,0 +1,10 @@
+LINKEDIN_MESSAGE_PROMPT = '''
+Estimado/a {nombre_de_la_empresa},
+
+Tu Nombre: {nombre}
+Tu Rol Actual: {rol_actual}
+Rol a postular: {rol_a_postular}
+Solicitud de Conversación: Pedir de manera cortés coordinar una conversación o reunión para profundizar una entrevista en la empresa.
+
+Saludos cordiales, {nombre}
+'''
